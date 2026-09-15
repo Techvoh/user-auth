fd80987fa39677df77222eb77c0ad82662e44200
Techvoh <calvinceadhere272@gmail.com>
2026-09-15 09:59:36 -0700

    added logins

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 
 const userRoute = express.Router()
-const {createUser, deleteUser, getAllUsers, getSingleUser, updateUser} = require(
+const {createUser, deleteUser, getAllUsers, getSingleUser, updateUser, loginUser} = require(
     "../controller/userController"
 )
 
@@ -11,5 +11,6 @@ userRoute.get("/all-users", getAllUsers)
 userRoute.get("/single-user/:id", getSingleUser)
 userRoute.put("/update-user/:id", updateUser)
 userRoute.delete("/delete-user/:id", deleteUser)
+userRoute.post("/login", loginUser)
 
 module.exports = userRoute
